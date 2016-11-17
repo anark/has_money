eec9cbeb0b550d9e6dea99fd23c4495805a94969
Andrew Narkewicz <anark6@gmail.com>
2016-11-16 17:46:02 -0800

    Add support for zero decimal currencies in Stripe

--- lib/has_money.rb
+++ lib/has_money.rb
@@ -12,23 +12,35 @@ module HasMoney
       attributes.each do |attribute|
         class_eval <<-EOS
           def #{attribute}_in_dollars
+            return #{attribute} if self.class.zero_decimal_currency?(currency)
             self.class.calculate_dollars_from_cents(#{attribute})
           end
 
           def #{attribute}_in_dollars=(dollars)
+            self.#{attribute} = dollars and return if self.class.zero_decimal_currency?(currency)
             self.#{attribute} = self.class.calculate_cents_from_dollars(dollars)
           end
 
           # Special dollars only formatting (no decimal place and rounded to the nearest dollar)
           def #{attribute}_in_dollars_without_cents
+            return self.#{attribute} if self.class.zero_decimal_currency?(currency)
             self.class.calculate_dollars_without_cents_from_cents(#{attribute})
           end
 
           def #{attribute}_in_dollars_without_cents=(dollars)
+            self.#{attribute} = dollars and return if self.class.zero_decimal_currency?(currency)
             self.#{attribute} = self.class.calculate_cents_from_dollars_without_cents(dollars)
           end
         EOS
       end
+    end
+
+    def zero_decimal_currencies
+      ['BIF', 'CLP', 'DJF', 'GNF', 'JPY', 'KMF', 'KRW', 'MGA', 'PYG', 'RWF', 'VND', 'VUV', 'XAF', 'XOF', 'XPF']
+    end
+
+    def zero_decimal_currency?(currency)
+      zero_decimal_currency.include?(currency.upcase)
     end
 
     def calculate_dollars_from_cents(cents)
